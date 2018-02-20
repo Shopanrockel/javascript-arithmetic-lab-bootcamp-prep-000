b6f270e12e569a1844a4d06c4031ee0aa03b3de6
Skip Harris <skip.harris@barclaycollege.edu>
2018-02-20 00:07:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,19 @@ function subtract(a, b) {
 function multiply(a, b) {
   return a * b
 }
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(a) {
+  return a++
+}
+
+function dec(a) {
+  return a--
+}
+
+function makeInt(a) {
+  return parseInt(a , 10)
+}
